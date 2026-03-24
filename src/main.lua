347c61c0865b83a423337803051f0a7ae048f7bb
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-03-24 05:04:38 -0500

    the basic love rendering loops

---
+++ src/main.lua
@@ -0,0 +1,15 @@
+if arg[#arg] == "debug" then
+    require("lldebugger").start()
+end
+function love.load()
+    --TODO: initialize things
+end
+function love.update(dt)
+    --TODO: update things
+end
+function love.draw()
+    --TODO: draw things
+end
+function love.keypressed( key, scancode, isrepeat )
+    --TODO: handle key presses
+end
