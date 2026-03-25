fffb115fd833bc691edb8406aeb39c6df7d09cf0
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-03-25 13:11:43 -0500

    we can transition states with a menu state. huzzah

--- src/aboutstate.lua
+++ src/aboutstate.lua
@@ -1,7 +1,30 @@
 local M = {}
+M.OK = "ok"
+M.CANCEL = "cancel"
 local menustate = require("menustate")
+local menu = require("menu")
+local menuitem = require("menuitem")
+local colors = require("colors")
+local states = require("states")
+local function createAboutMenuHandler(state)
+    local result = menu.create(0,0,"About", colors.BROWN, colors.BLACK)
+    result:addItem(menuitem.create(M.OK,"OK", colors.WHITE, colors.BLACK))
+    result:addItem(menuitem.create(M.CANCEL,"Cancel", colors.WHITE, colors.BLACK))
+    return result
+end
+local function aboutMenuItemHandler(state, itemId)
+    if itemId == M.OK then
+        return states.TITLE
+    elseif itemId == M.CANCEL then
+        return states.TITLE
+    end
+    return nil
+end
+local function aboutCancelHandler(state)
+    return nil
+end
 function M.create()
-    local instance = menustate.create()
+    local instance = menustate.create(createAboutMenuHandler, aboutMenuItemHandler, aboutCancelHandler)
     return instance
 end
 return M
--- src/menustate.lua
+++ src/menustate.lua
@@ -1,20 +1,57 @@
 local M = {}
 local state = require("state")
+local framebuffer = require("framebuffer")
+local constants = require("constants")
+local colors = require("colors")
+local commands = require("commands")
 local function menuStateStartHandler(state)
+    framebuffer.fill(0, 0, constants.CELL_COLUMNS, constants.CELL_ROWS, 1, colors.BLACK, colors.BLACK)
+    state.menu = state:createMenu()
 end
 local function menuStateStopHandler(state)
 end
 local function menuStateUpdateHandler(state, dt)
+    state.menu:render()
 end
 local function menuStateCommandHandler(state, command)
+    if command == commands.UP then
+        state.menu:previousItem()
+    elseif command == commands.DOWN then
+        state.menu:nextItem()
+    elseif command == commands.GREEN then
+        return state:handleMenuItem(state.menu:getCurrentItemId())
+    elseif command == commands.RED then
+        return state:handleCancel()
+    end
     return nil
 end
-function M.create()
+function M.create(createMenuHandler,menuItemHandler, cancelHandler)
     local instance = state.create(
         menuStateStartHandler, 
         menuStateStopHandler, 
         menuStateUpdateHandler, 
         menuStateCommandHandler)
+    instance.createMenuHandler = createMenuHandler
+    instance.menuItemHandler = menuItemHandler
+    instance.cancelHandler = cancelHandler
+    function instance:createMenu()
+        if self.createMenuHandler ~= nil then
+            return self:createMenuHandler()
+        end
+        return nil
+    end
+    function instance:handleMenuItem(itemId)
+        if self.menuItemHandler ~= nil then
+            return self:menuItemHandler(itemId)
+        end
+        return nil
+    end
+    function instance:handleCancel()
+        if self.cancelHandler ~= nil then
+            return self:cancelHandler()
+        end
+        return nil
+    end
     return instance
 end
 return M
